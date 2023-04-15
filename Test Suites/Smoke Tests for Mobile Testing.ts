b05a4db00765e917f59ad924c431fa1cc00d2fff
Aldy Syah <aldy.ramadhan@tokopedia.com>
2023-04-15 13:51:24 +0700

    commit: add config for TS run

--- Test Suites/Smoke Tests for Mobile Testing.ts
+++ Test Suites/Smoke Tests for Mobile Testing.ts
@@ -8,9 +8,19 @@
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
          <configuration>
-            <groupName>Web Desktop</groupName>
+            <groupName>Mobile</groupName>
             <profileName>default</profileName>
-            <runConfigurationId>Chrome</runConfigurationId>
+            <runConfigurationData>
+               <entry>
+                  <key>deviceName</key>
+                  <value>emulator-5554 (sdk_gphone_arm64 - Android 11)</value>
+               </entry>
+               <entry>
+                  <key>deviceId</key>
+                  <value>emulator-5554</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>Android</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Smoke Tests for Mobile Browsers</testSuiteEntity>
